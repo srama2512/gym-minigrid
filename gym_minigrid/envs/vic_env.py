8221bea052d23df9895b339ea9d8fee293b57320
Santhosh Kumar Ramakrishnan <sramakri@fb.com>
2018-07-30 17:24:41 -0700

    added randomize for vic env

--- gym_minigrid/envs/vic_env.py
+++ gym_minigrid/envs/vic_env.py
@@ -1,13 +1,16 @@
 from gym_minigrid.minigrid import *
 from gym_minigrid.register import register
+import random
 
 class VICEnv(MiniGridEnv):
     """
     Environment with 10x6 dimensions (9x5 effective states) 
     """
 
-    def __init__(self, mode='rgb', action_mode='forward'):
+    def __init__(self, mode='rgb', action_mode='forward', randomize=False):
 
+        self.randomize = randomize
+        self.valid_starts = [(x, y) for x in range(1,6) for y in range(1, 10)]
         super(VICEnv, self).__init__(
             grid_size=11,
             max_steps=20000,
@@ -31,8 +34,11 @@ class VICEnv(MiniGridEnv):
         for i in range(0, width):
             self.grid.set(i, 0, wall)
             self.grid.set(i, height-1, wall) 
-
-        self.start_pos = (3, 5)
+        
+        if self.randomize:
+            self.start_pos = random.sample(self.valid_starts, 1)[0]
+        else:
+            self.start_pos = (3, 5)
         self.start_dir = 0
 
         self.mission = 'just roam around'
@@ -48,6 +54,22 @@ class VICEnvOmni(VICEnv):
 class VICGridEnvOmni(VICEnv):
     def __init__(self):
         super().__init__(mode='grid', action_mode='omni')
+
+class VICEnvRandomize(VICEnv):
+    def __init__(self):
+        super().__init__(randomize=True)
+
+class VICGridEnvRandomize(VICEnv):
+    def __init__(self):
+        super().__init__(mode='grid', randomize=True)
+
+class VICEnvOmniRandomize(VICEnv):
+    def __init__(self):
+        super().__init__(mode='rgb', action_mode='omni', randomize=True)
+
+class VICGridEnvOmniRandomize(VICEnv):
+    def __init__(self):
+        super().__init__(mode='grid', action_mode='omni', randomize=True)
 
 register(
     id='MiniGrid-VICEnv-v0',
@@ -67,5 +89,25 @@ register(
 register(
     id='MiniGrid-VICEnv-v3',
     entry_point='gym_minigrid.envs:VICGridEnvOmni',
+    reward_threshold=1000.0
+)
+register(
+    id='MiniGrid-VICEnv-Randomize-v0',
+    entry_point='gym_minigrid.envs:VICEnvRandomize',
+    reward_threshold=1000.0
+)
+register(
+    id='MiniGrid-VICEnv-Randomize-v1',
+    entry_point='gym_minigrid.envs:VICGridEnvRandomize',
+    reward_threshold=1000.0
+)
+register(
+    id='MiniGrid-VICEnv-Randomize-v2',
+    entry_point='gym_minigrid.envs:VICEnvOmniRandomize',
+    reward_threshold=1000.0
+)
+register(
+    id='MiniGrid-VICEnv-Randomize-v3',
+    entry_point='gym_minigrid.envs:VICGridEnvOmniRandomize',
     reward_threshold=1000.0
 )
