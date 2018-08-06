b274c1f9497949685536b34a030669da16cef36f
Santhosh Kumar Ramakrishnan <sramakri@fb.com>
2018-08-06 14:34:01 -0700

    updated multiroom simple to have randomized starts, omni actions

--- gym_minigrid/envs/multiroom_simple.py
+++ gym_minigrid/envs/multiroom_simple.py
@@ -7,21 +7,21 @@ class MultiRoomSimpleEnv(MiniGridEnv):
     Environment with multiple rooms (no doors)
     """
 
-    def __init__(self, randomize=False, mode='rgb'):
+    def __init__(self, randomize=False, mode='rgb', action_mode='forward'):
 
+        self.randomize = randomize
         super(MultiRoomSimpleEnv, self).__init__(
-            grid_size=27,
+            grid_size=11,
             max_steps=20000,
             see_through_walls=True,
-            mode=mode
+            mode=mode,
+            action_mode=action_mode
         )
-        # TODO
-        self.randomize = randomize
 
     def _gen_grid(self, width, height, start_pos=None, start_dir=None):
         
-        width = 27 # hard-coded
-        height = 27
+        width = 11 # hard-coded
+        height = 11
         self.grid = Grid(width, height)
         wall = Wall()
 
@@ -37,15 +37,48 @@ class MultiRoomSimpleEnv(MiniGridEnv):
             if i != height//4 and  i != (3*height)//4:
                 self.grid.set(i, height//2, wall)
             self.grid.set(i, height-1, wall) 
+        
+        self.valid_starts = []
+        for x in range(0, width-1):
+            for y in  range(1, height-1):
+                if self.grid.get(x, y) is None:
+                    self.valid_starts.append((x, y))
 
-        self.start_pos = (12, 12)
+        if self.randomize:
+            self.start_pos = random.sample(self.valid_starts, 1)[0]
+        else:    
+            self.start_pos = (4, 4)
         self.start_dir = 0
 
         self.mission = 'just roam around'
 
-class MultiRoomSimpleEnvGrid(MultiRoomSimpleEnv):
+class MultiRoomSimpleEnvOmni(MultiRoomSimpleEnv):
     def __init__(self):
-        super().__init__(mode='grid')
+        super().__init__(mode='rgb', action_mode='omni', randomize=False)
+
+class MultiRoomSimpleGridEnv(MultiRoomSimpleEnv):
+    def __init__(self):
+        super().__init__(mode='grid', action_mode='forward', randomize=False)
+
+class MultiRoomSimpleGridEnvOmni(MultiRoomSimpleEnv):
+    def __init__(self):
+        super().__init__(mode='grid', action_mode='omni', randomize=False)
+
+class MultiRoomSimpleEnvRandomize(MultiRoomSimpleEnv):
+    def __init__(self):
+        super().__init__(mode='rgb', action_mode='forward', randomize=True)
+
+class MultiRoomSimpleEnvOmniRandomize(MultiRoomSimpleEnv):
+    def __init__(self):
+        super().__init__(mode='rgb', action_mode='omni', randomize=True)
+
+class MultiRoomSimpleGridEnvRandomize(MultiRoomSimpleEnv):
+    def __init__(self):
+        super().__init__(mode='grid', action_mode='forward', randomize=True)
+
+class MultiRoomSimpleGridEnvOmniRandomize(MultiRoomSimpleEnv):
+    def __init__(self):
+        super().__init__(mode='grid', action_mode='omni', randomize=True)
 
 register(
     id='MiniGrid-MultiRoom-Simple-v0',
@@ -54,6 +87,36 @@ register(
 )
 register(
     id='MiniGrid-MultiRoom-Simple-v1',
-    entry_point='gym_minigrid.envs:MultiRoomSimpleEnvGrid',
+    entry_point='gym_minigrid.envs:MultiRoomSimpleGridEnv',
+    reward_threshold=1000.0
+)
+register(
+    id='MiniGrid-MultiRoom-Simple-v2',
+    entry_point='gym_minigrid.envs:MultiRoomSimpleEnvOmni',
+    reward_threshold=1000.0
+)
+register(
+    id='MiniGrid-MultiRoom-Simple-v3',
+    entry_point='gym_minigrid.envs:MultiRoomSimpleGridEnvOmni',
+    reward_threshold=1000.0
+)
+register(
+    id='MiniGrid-MultiRoom-Simple-Randomize-v0',
+    entry_point='gym_minigrid.envs:MultiRoomSimpleEnvRandomize',
+    reward_threshold=1000.0
+)
+register(
+    id='MiniGrid-MultiRoom-Simple-Randomize-v1',
+    entry_point='gym_minigrid.envs:MultiRoomSimpleGridEnvRandomize',
+    reward_threshold=1000.0
+)
+register(
+    id='MiniGrid-MultiRoom-Simple-Randomize-v2',
+    entry_point='gym_minigrid.envs:MultiRoomSimpleEnvOmniRandomize',
+    reward_threshold=1000.0
+)
+register(
+    id='MiniGrid-MultiRoom-Simple-Randomize-v3',
+    entry_point='gym_minigrid.envs:MultiRoomSimpleGridEnvOmniRandomize',
     reward_threshold=1000.0
 )
